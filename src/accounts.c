ca6774bc29d4b7d3848e382a1c3c244f971272eb
DogWaterDev <dogwaterdev@xpsystems.eu>
2026-01-27 11:47:44 +0100

    Initial commit. Added basic account functionality, owners don't exist yet and neither do transactions. Small example is given when you run the code

---
+++ src/accounts.c
@@ -0,0 +1,121 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <uuid/uuid.h>
+#include "accounts.h"
+
+static account_t *account_list;
+static size_t account_count = 0;
+static size_t account_list_size = 0;
+
+static void grow_owners(void) {
+	account_list_size = account_list_size == 0 ? 8 : account_list_size * 2;
+account_list = realloc(account_list, account_list_size * sizeof(account_t));
+
+	}
+
+static int get_account_index(uuid_t account_uuid) {
+	for (size_t i = 0; i < account_count; i++) {
+		if (memcmp(account_list[i].uuid, account_uuid, sizeof(uuid_t)) == 0) {
+			return (int)i;
+		}
+	}
+	return (-1);
+}
+
+void add_account(uuid_t *account_uuid_container, const char *name) {
+	if (account_count+1 > account_list_size) {
+		grow_owners();
+	}
+
+	account_t *tmp = &(account_list[account_count == 0 ? 0 : account_count-1]);
+	
+	tmp->balance = 0;
+	uuid_generate_random(tmp->uuid);
+	tmp->frozen = 0;
+	tmp->negative = 0;
+	strncpy(tmp->name, name, ACCOUNT_NAME_LIMIT-1);
+	tmp->name[ACCOUNT_NAME_LIMIT-1] = '\0';
+	memcpy(*account_uuid_container, tmp->uuid, sizeof(tmp->uuid));
+	account_count++;
+}
+
+uint64_t get_account_balance(uuid_t account_uuid) {
+	int index = get_account_index(account_uuid);
+	if (index != -1) {
+		return account_list[index].balance;
+	}
+	return (uint64_t)(-1); // if someone reaches this amount normally just fuck them ngl
+}
+
+int set_account_balance(uuid_t account_uuid, uint64_t balance) {
+	int index = get_account_index(account_uuid);
+	if (index != -1) {
+		account_list[index].balance = balance;
+		return 0;
+	}
+	return -1;
+}
+
+void get_account_owner_uuid(uuid_t *owner_uuid_container, uuid_t account_uuid) {
+	int index = get_account_index(account_uuid);
+	if (index == -1) {
+		return;
+	}
+	memcpy(*owner_uuid_container, account_list[index].owner_uuid, sizeof(uuid_t));
+}
+
+int set_account_owner_uuid(uuid_t account_uuid, uuid_t owner_uuid) {
+	int index = get_account_index(account_uuid);
+	if (index == -1) {
+		return -1;
+	}
+	memcpy(account_list[index].owner_uuid, owner_uuid, sizeof(uuid_t));
+	return 0;
+}
+
+int get_account_frozen(uuid_t account_uuid) {
+	int index = get_account_index(account_uuid);
+	if (index != -1) {
+		return account_list[index].frozen;	
+	}
+	return -1;
+}
+
+int set_account_frozen(uuid_t account_uuid, int frozen) {
+	int index = get_account_index(account_uuid);
+	if (index != -1) {
+		account_list[index].frozen = frozen;
+		return 0;
+	}
+	return -1;
+}
+
+int get_account_negative(uuid_t account_uuid) {
+	int index = get_account_index(account_uuid);
+	if (index != -1) {
+		return account_list[index].negative;	
+	}
+	return -1;
+}
+
+int set_account_negative(uuid_t account_uuid, int negative) {
+	int index = get_account_index(account_uuid);
+	if (index != -1) {
+		account_list[index].negative = negative;
+		return 0;
+	}
+	return -1;
+}
+
+
+void free_accounts(void) {
+/*	for (size_t i = 0; i < account_count; i++) {
+		free(account_list[i].uuid);
+	}
+*/
+	free(account_list);
+}
+
+
---
+++ src/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <uuid/uuid.h>
+#include <stdlib.h>
+
+#include "accounts.h"
+
+
+int main() {
+
+	const char *account_name = "Santa's Treasure";
+	const uint64_t new_balance = (uint64_t)1000;
+	
+	char account_uuid_str[37];
+	uuid_t account_uuid;
+
+	printf("Making an account with the name %s\n", account_name);
+	add_account(&account_uuid, account_name);
+	uuid_unparse(account_uuid, account_uuid_str);
+	printf("\nIts UUID is: %s\n", account_uuid_str);
+	
+	printf("Current account balance: %ld\n", get_account_balance(account_uuid));
+	printf("Changing balance to %ld\n", new_balance);
+	set_account_balance(account_uuid, new_balance);
+	printf("New account balance: %ld\n", get_account_balance(account_uuid));
+
+	free_accounts();
+	return 0;
+}
